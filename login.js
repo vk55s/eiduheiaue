9a108634ff6b1767f51ef9b0cdcf67747e590bf8
vk <101263463+vk55s@users.noreply.github.com>
2024-11-30 18:00:51 +0800

    更新 login.js

--- login.js
+++ login.js
@@ -3,7 +3,6 @@ const passwordInput = document.getElementById('password-input');
 const errorMessage = document.getElementById('error-message');
 const container = document.querySelector('.container');
 
-// 正确密码（可自行修改）
 const correctPassword = '4586';
 
 passwordForm.addEventListener('submit', (e) => {
@@ -12,15 +11,13 @@ passwordForm.addEventListener('submit', (e) => {
     const enteredPassword = passwordInput.value.trim();
 
     if (enteredPassword === correctPassword) {
-        // 正确密码，跳转到密钥页面
         window.location.href = 'miyao.html';
     } else {
-        // 密码错误，显示错误提示并震动容器
-        errorMessage.style.display = 'block';  // 显示错误信息
+        errorMessage.style.display = 'block';
         container.classList.add('shake');
 
         setTimeout(() => {
-            errorMessage.style.display = 'none';  // 隐藏错误信息
+            errorMessage.style.display = 'none';
             container.classList.remove('shake');
         }, 1500);
     }
